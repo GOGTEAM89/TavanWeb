465bb8138687140c82756d39f504bb75b8360294
GOGTEAM89 <Arashtahmasebi449@yahoo.com>
2017-05-05 09:49:12 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -95,7 +95,7 @@ function create_config( )
 	"lock-fosh"
 	
  },
-    sudo_users = {338345804,245852578,0}, --ID shoma 
+    sudo_users = {338345804,245852578}, --ID shoma 
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
